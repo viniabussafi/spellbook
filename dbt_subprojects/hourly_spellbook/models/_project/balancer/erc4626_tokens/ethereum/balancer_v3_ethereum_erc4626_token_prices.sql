96c390cf0566997c5beaba1a8c3ae30d7973b447
viniabussafi <vinicius@balancerlabs.dev>
2024-12-13 18:29:15 +0000

    make incremental files into tables

--- dbt_subprojects/hourly_spellbook/models/_project/balancer/erc4626_tokens/ethereum/balancer_v3_ethereum_erc4626_token_prices.sql
+++ dbt_subprojects/hourly_spellbook/models/_project/balancer/erc4626_tokens/ethereum/balancer_v3_ethereum_erc4626_token_prices.sql
@@ -1,11 +1,8 @@
 {{ config(
         schema = 'balancer_v3_ethereum',
         alias = 'erc4626_token_prices',
-        materialized = 'incremental',
-        file_format = 'delta',
-        incremental_strategy = 'merge',
-        unique_key = ['minute', 'wrapped_token'],
-        incremental_predicates = [incremental_predicate('DBT_INTERNAL_DEST.minute')]
+        materialized = 'table',
+        file_format = 'delta'
     )
 }}
 
@@ -15,9 +12,6 @@ WITH wrap_unwrap AS(
             wrappedToken,
             CAST(depositedUnderlying AS DOUBLE) / CAST(mintedShares AS DOUBLE) AS ratio
         FROM {{ source('balancer_v3_ethereum', 'Vault_evt_Wrap') }}
-        {% if is_incremental() %}
-        WHERE {{ incremental_predicate('evt_block_time') }}
-        {% endif %}      
 
         UNION ALL
 
@@ -25,10 +19,7 @@ WITH wrap_unwrap AS(
             evt_block_time,
             wrappedToken, 
             CAST(withdrawnUnderlying AS DOUBLE) / CAST(burnedShares AS DOUBLE) AS ratio
-        FROM {{ source('balancer_v3_ethereum', 'Vault_evt_Unwrap') }}    
-        {% if is_incremental() %}
-        WHERE {{ incremental_predicate('evt_block_time') }}
-        {% endif %}      
+        FROM {{ source('balancer_v3_ethereum', 'Vault_evt_Unwrap') }}        
     ),
 
 
@@ -45,10 +36,7 @@ WITH wrap_unwrap AS(
     JOIN {{ref('balancer_v3_ethereum_erc4626_token_mapping')}} m ON m.erc4626_token = w.wrappedToken
     JOIN {{ source('prices', 'usd') }} p ON m.underlying_token = p.contract_address
     AND p.blockchain = 'ethereum'
-    AND DATE_TRUNC('minute', w.evt_block_time) = DATE_TRUNC('minute', p.minute)
-    {% if is_incremental() %}
-    AND {{ incremental_predicate('p.minute') }}
-    {% endif %}            
+    AND DATE_TRUNC('minute', w.evt_block_time) = DATE_TRUNC('minute', p.minute)         
     )
 
 SELECT
@@ -58,8 +46,8 @@ SELECT
     underlying_token,
     erc4626_token_symbol,
     underlying_token_symbol,
- --   decimals,
+    decimals,
     APPROX_PERCENTILE(adjusted_price, 0.5) AS median_price,
     LEAD(p.evt_block_time, 1, CURRENT_DATE + INTERVAL '1' day) OVER (PARTITION BY wrappedToken ORDER BY p.evt_block_time) AS next_change
 FROM price_join p
-GROUP BY 1, 2, 3, 4, 5, 6--, 7
+GROUP BY 1, 2, 3, 4, 5, 6, 7
